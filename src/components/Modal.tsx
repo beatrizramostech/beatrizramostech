53a20ae601bbf46c859410a3f1adc9978304046d
Beatriz Ramos Tech <beatrizramosdesenvolvedora@gmail.com>
2024-07-11 11:44:45 -0300

    :construction: Home Page

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,29 @@
-const Modal: React.FC<{ isOpen: boolean, onClose: () => void, content: { title: string, description: string[] } }> = ({ isOpen, onClose, content }) => {
+import { XCircle } from "@phosphor-icons/react";
+
+const Modal: React.FC<{
+  isOpen: boolean;
+  onClose: () => void;
+  content: { title: string; description: string[] };
+}> = ({ isOpen, onClose, content }) => {
   if (!isOpen) return null;
-  
+
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
+    <div
+      className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
       <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
-        <h2 className="text-xl mb-4">{content.title}</h2>
+        <div className="flex flex-row items-center justify-between mb-4">
+          <h2 className="text-xl">{content.title}</h2>
+          <button onClick={onClose} className="text-red">
+            <XCircle size={32} />
+          </button>
+        </div>
         <ul className="list-green-disc pl-5 space-y-2">
           {content.description.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
-        <button onClick={onClose} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Fechar</button>
       </div>
     </div>
   );
