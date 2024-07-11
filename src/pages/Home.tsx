af1c47ac7a82909693b64656a8f817a591378832
Beatriz Ramos Tech <beatrizramosdesenvolvedora@gmail.com>
2024-07-11 12:32:53 -0300

    :lipstick: Logo resizing and personalized styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,15 +16,15 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-around min-h-screen w-full p-4">
-      <div className="flex flex-col items-center lg:items-start my-4 lg:my-4">
+      <div className="flex flex-col items-center lg:items-start">
           <img
             src={logo}
             alt="Logo Beatriz Ramos Tech"
-            className="w-56 h-56 lg:w-128 lg:h-128"
+            className="w-60 h-60 lg:w-128 lg:h-128"
           />
       </div>
 
-      <div className="mt-4 lg:mt-0 lg:ml-16">
+      <div className="mt-4 lg:mt-0">
         <div className="bg-gradient-to-r from-gradient-purple to-gradient-orange rounded-xl p-4 lg:p-8 shadow-md">
           <h2 className="text-xl lg:text-2xl text-center lg:text-left text-gray-800 mb-4">Nossos Serviços:</h2>
           <ul className="space-y-4">
